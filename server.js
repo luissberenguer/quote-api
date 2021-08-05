14cc1ef01aa834f6647bfabb3d9a307ea5855606
Luis Berenguer <luis01developer@gmail.com>
2021-08-06 00:38:50 +0200

    Add POST to /api/quotes

--- server.js
+++ server.js
@@ -8,37 +8,45 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+
 app.get('/api/quotes/random', (req, res, next) => {
     let rElement = getRandomElement(quotes);
-    let rQuote = {
-        quote: rElement
-    }
+    let rQuote = {quote: rElement}
     res.status(200).send(rQuote);
 })
 
 
 app.get('/api/quotes', (req, res, next) => {
     let person = req.query.person;
-    
-    if(!person){
-        let allQuotes = {
-            quotes: quotes
-        }
-        res.send(allQuotes);
-    } else {
-        
+    let newQuote = req.query.quote;
+
+    if(person){
         let personQuotes = [];
         quotes.forEach(obj => {
-            if(obj.person === person){
+            if(obj.person.toLowerCase() === person.toLowerCase()){
                 personQuotes.push(obj);
             }
         })
-        res.send({
-            quotes: personQuotes   
-        })
+        res.status(200).send({quotes: personQuotes})
+    } else if(!newQuote){
+        let allQuotes = {quotes: quotes}
+        res.status(200).send(allQuotes);
     }
-
 })
+
+app.post('/api/quotes', (req, res, next) => {
+    let newQuote = req.query.quote;
+    let newPerson = req.query.person;
+    
+    if(newQuote && newPerson){
+        let newElement = {quote: newQuote, person: newPerson};
+        quotes.push(newElement);
+        res.status(201).send({quote: newElement});
+    } else {
+        res.status(400).send();
+    }
+})
+
 
 
 app.listen(PORT, () => {
