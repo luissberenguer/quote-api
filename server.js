588955b225243497dc888c061989ebbe8f465978
Luis Berenguer <luis01developer@gmail.com>
2021-08-06 01:29:00 +0200

    Implement CodeCademy tips for better code

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const { quotes } = require('./data');
@@ -8,45 +9,52 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
-
 app.get('/api/quotes/random', (req, res, next) => {
-    let rElement = getRandomElement(quotes);
-    let rQuote = {quote: rElement}
-    res.status(200).send(rQuote);
+    const quote = getRandomElement(quotes);
+    res.send({quote: quote})
 })
 
-
 app.get('/api/quotes', (req, res, next) => {
-    let person = req.query.person;
-    let newQuote = req.query.quote;
-
-    if(person){
-        let personQuotes = [];
-        quotes.forEach(obj => {
-            if(obj.person.toLowerCase() === person.toLowerCase()){
-                personQuotes.push(obj);
-            }
-        })
-        res.status(200).send({quotes: personQuotes})
-    } else if(!newQuote){
-        let allQuotes = {quotes: quotes}
-        res.status(200).send(allQuotes);
+    if(!req.query.hasOwnProperty('person')){
+        res.send({quote: quotes})
+    } else {
+        const filterQuote = quotes.filter(element => element.person === req.query.person);
+        res.send({quotes: filterQuote});
     }
 })
 
 app.post('/api/quotes', (req, res, next) => {
-    let newQuote = req.query.quote;
-    let newPerson = req.query.person;
-    
-    if(newQuote && newPerson){
-        let newElement = {quote: newQuote, person: newPerson};
-        quotes.push(newElement);
-        res.status(201).send({quote: newElement});
-    } else {
-        res.status(400).send();
+    if(req.query.quote && req.query.person){
+        const newQuote = {quote: req.query.quote, person: req.query.person};
+        quotes.push(newQuote);
+        res.send({quote: newQuote})
+    } 
+    else {
+        res.status(400).send()
     }
 })
 
+// Update one quote
+app.post('/api/quotes:id', (req, res, next) => {
+    if((req.query.person && req.query.quote) && quotes[req.params.id]){
+        let newQuote = {quote: req.query.quote, person: req.query.person};
+        quotes[req.params.id] = newQuote;
+        send.status(200).send();
+    }
+    else {
+        res.status(400).send()
+    }
+}) 
+
+// Delete one quote
+app.post('/api/quotes:id', (req, res, next) => {
+    if(quotes[req.params.id]){
+        quotes.splice(req.params.id, 1)
+        res.status(204).send();
+    } else {
+        res.status(404).send()
+    }
+})
 
 
 app.listen(PORT, () => {
